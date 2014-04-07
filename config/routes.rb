3ba9bd187a085164d54fa29f030b33e2b4197453
Joshua Rose <joshuarose@outlook.com>
2014-04-06 23:03:40 -0400

    Finish the signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-
+  resources :users
   get "users/new"
   root "static_pages#home"
   match '/help',    to: 'static_pages#help', :via => [:get], :as => "help"
